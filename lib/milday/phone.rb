c1fd3d969c4f1288624c534ec3353ffa39c2ffff
rockwellll <dev.ahmad.khattab@gmail.com>
2024-08-11 10:29:02 +0300

    add a global test_helper

--- lib/milday/phone.rb
+++ lib/milday/phone.rb
@@ -1,9 +1,11 @@
 module Milday
   class Phone
+    VALID_TYPES = %w[text voice fax cell video pager textphone].freeze
+    
     def self.from_s(data)
       data = data[data.index("TEL;")..] unless data.starts_with?("TEL;")
       identifier = data.split(":").last
-      options = data.gsub("TEL", "").split(":").first.split(";").compact_blank.map { _1.split("=") }.to_h
+      options = data.gsub("TEL", "").split(":").first.split(";").map { _1.split("=") }.to_h
       
       new(identifier, **options)
     end
@@ -11,6 +13,9 @@ module Milday
     def initialize(tel, **options)
       @tel = tel
       @options = options
+      @type = @options[:type] || 'voice'
+      
+      raise ArgumentError, 'Invalid Position' if position? && @options[:position].to_i < 1
     end
     
     def [](key)
@@ -18,15 +23,37 @@ module Milday
     end
     
     def to_s
-      "TEL;#{@options.reject { |key, _| %i[type].exclude?(key)} .map { "#{_1}=#{_2}" }.join(";")}:#{@tel}"
+      parts = ['TEL']
+      
+      parts << "PERF=#{@options[:position]}" if position?
+      parts << type_to_s if type?
+      
+      parts.join(';') + ":#{@tel}"
     end
     
     def to_h
       {
         number: @tel,
-        type: @options[:type],
-        preferred: @options[:preferred],
+        type: @type,
+        position: @options[:position],
       }
     end
+    
+    private
+      def position?
+        @options[:position]
+      end
+    
+      def type?
+        @options[:type]
+      end
+      
+      def type_to_s
+        if @options[:type].is_a?(Array)
+          "TYPE=\"#{@options[:type].join(',')}\""
+        else
+          "TYPE=#{@options[:type]}"
+        end
+      end
   end
 end
