45dacac6bb74663766035c93f1ac6eae8e024b9b
rockwellll <dev.ahmad.khattab@gmail.com>
2024-08-11 11:35:31 +0300

    expand gender to use Sex component

--- lib/milday/gender.rb
+++ lib/milday/gender.rb
@@ -1,5 +1,13 @@
 module Milday
   class Gender
+    SEX_COMPONENTS = {
+      male: 'M',
+      female: 'F',
+      other: 'O',
+      unknown: 'U',
+      none: 'N'
+    }
+    
     attr_reader :identifier
     
     def self.from_s(data)
@@ -7,7 +15,13 @@ module Milday
     end
     
     def initialize(identifier)
-      @identifier = identifier
+      if SEX_COMPONENTS.keys.include?(identifier.to_s.downcase.to_sym)
+        @identifier = SEX_COMPONENTS[identifier.to_s.downcase.to_sym]
+      else
+        @identifier = identifier
+      end
+      
+      raise ArgumentError, 'Invalid Sex identifier' unless SEX_COMPONENTS.values.include?(@identifier)
     end
     
     def to_s
@@ -16,8 +30,13 @@ module Milday
     
     def to_h
       {
-        identifier: @identifier
+        identifier: @identifier,
+        sex:,
       }
+    end
+    
+    def sex
+      @sex ||= SEX_COMPONENTS.key(@identifier)
     end
   end
 end
--- test/gender_test.rb
+++ test/gender_test.rb
@@ -9,6 +9,21 @@ class GenderTest < Minitest::Test
     end
   end
   
+  def test_initialize_with_valid_sex_component_key
+    assert_runs_without_errors do
+      Milday::Gender.new('male').tap do |entry|
+        assert_equal(entry.identifier, 'M')
+        assert_equal(entry.sex, :male)
+      end
+    end
+  end
+  
+  def test_initialize_with_invalid_identifier
+    assert_raises(ArgumentError) do
+      Milday::Gender.new('invalid-identifier')
+    end
+  end
+  
   def test_to_s
     assert_equal(Milday::Gender.new('M').to_s, 'GENDER:M')
   end
